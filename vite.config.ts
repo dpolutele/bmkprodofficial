3dfcaa653fd6150ef5b7a287b679e89bd96d3091
dpolutele <128649606+dpolutele@users.noreply.github.com>
2025-10-28 07:55:35 +1100

    Deploy site to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bmkprodofficial/", // ✅ Chemin pour GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
     fs: {
-      allow: [".","./client", "./shared"],
+      allow: [".", "./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
+
   build: {
     outDir: "dist/spa",
   },
+
   plugins: [react(), expressPlugin()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
@@ -28,11 +32,9 @@ export default defineConfig(({ mode }) => ({
 function expressPlugin(): Plugin {
   return {
     name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
+    apply: "serve", // Only apply during dev
     configureServer(server) {
       const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
       server.middlewares.use(app);
     },
   };
